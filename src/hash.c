44b0e637f3f75ea390676005e17b0ed00bdcb82f
Lee Yeonji <liyeonji@gmail.com>
2019-12-23 17:32:49 +0900

    Add insert method for Hash class

--- src/hash.c
+++ src/hash.c
@@ -221,6 +221,157 @@ int uol_rbtree_compare(uol_object_t * key1, uol_object_t * key2)
 #endif /* UOL_HASH_NUMERICKEY */
 }
 
+/**
+ *  uol_rbtree_insert -- insert item into the uol hash rb-tree
+ *
+ * The uol_rbtree_insert() is used to insert one item into the uol hash RB-Tree.
+ * 
+ * The uol_rbtree_insert() function has no return value. It will always succeed.
+ */
+void uol_rbtree_insert(uol_hash_t * tree, uol_hash_node_t * node)
+{
+	uol_hash_node_t * uncle;
+
+	if (tree->root == tree->nil) { /* A empty tree */
+		node->left   = tree->nil;
+		node->right  = tree->nil;
+		node->parent = tree->nil;
+		uol_rbtree_set_black(node);
+
+		tree->root = node;
+
+		return;
+	}
+
+	{ /* Insert the node into a leaf */
+		uol_hash_node_t * last; /* the last branch node in the tree */
+		uol_hash_node_t ** pos; /* the leaf to be replaced */
+
+		last = tree->root;
+
+		/* Find the leaf to replace */
+		while (1) {
+			if(uol_rbtree_compare(node->key, last->key) > 0) {
+				pos = &last->left;
+			} else {
+				pos = &last->right;
+			}
+
+			if (*pos == tree->nil) {
+				break;
+			}
+
+			last = *pos;
+		}
+
+		/* replace the leaf */
+		*pos = node;
+		node->parent = last;
+		node->left   = tree->nil;
+		node->right  = tree->nil;
+
+		/* the new node is red */
+		uol_rbtree_set_red(node);
+
+	} /* End insert the node into a leaf */
+
+	/* re-balance the tree */
+	while (node != tree->root && uol_rbtree_is_red(node->parent)) {
+		/* if the parent node is red, no need to adjust this tree */
+
+		/**
+		 *    GrandParent
+		 *      /     \ 
+		 *   Parent  Uncle
+		 *    /
+		 *  node
+		 */
+
+		/* Check parent is at which side of grandparent */
+		if (node->parent == node->parent->parent->left) {
+			uncle = node->parent->parent->right;
+
+			if (uol_rbtree_is_red(uncle)) {
+				/* If parent is red and uncle also red.
+				 *
+				 *      [B]GrandParent
+				 *      /        \ 
+				 *    [R]Parent  [R]Uncle
+				 *    /
+				 *  [R]node
+				 */
+
+				/* Re-color P and U to black, and GP to red */
+				uol_rbtree_set_black(node->parent);
+				uol_rbtree_set_black(uncle);
+				uol_rbtree_set_red(node->parent->parent);
+
+				/* Recursion with GP */
+				node = node->parent->parent;
+
+			} else {
+				/* If parent is red and uncle also red.
+				 *
+				 *      [B]GrandParent
+				 *      /        \ 
+				 *    [R]Parent  [B]Uncle
+				 *    /
+				 *  [R]node
+				 */
+				if (node == node->parent->right) {
+					/* Confirm the node is the left leaf of parent */
+					/* Rotating to switch two node to keep in order */
+					node = node->parent;
+					uol_rbtree_rotateL(tree, node);
+				}
+
+				uol_rbtree_set_black(node->parent);
+				uol_rbtree_set_red(node->parent->parent);
+				uol_rbtree_rotateR(tree, node->parent->parent);
+
+				/* After this will be
+				 *
+				 *    [B]Parent
+				 *    /      \ 
+				 *  [R]node  [R]GrandParent
+				 *             \
+				 *             [B]Uncle
+				 *
+				 * Exit loop because node->parent is black
+				 */
+			}
+
+		} else {
+			/* Same as before, just change direction */
+			uncle = node->parent->parent->left;
+
+			if (uol_rbtree_is_red(uncle)) {
+				uol_rbtree_set_black(node->parent);
+				uol_rbtree_set_black(uncle);
+				uol_rbtree_set_red(node->parent->parent);
+
+				node = node->parent->parent;
+
+			} else {
+				if (node == node->parent->left) {
+					node = node->parent;
+					uol_rbtree_rotateR(tree, node);
+				}
+
+				uol_rbtree_set_black(node->parent);
+				uol_rbtree_set_red(node->parent->parent);
+				uol_rbtree_rotateL(tree, node->parent->parent);
+			}
+
+		}
+
+	}
+
+	/* Confirm the root is black */
+	uol_rbtree_set_black(tree->root);
+}
+
+
 int main()
 {
 	uol_hash_t *hash;
