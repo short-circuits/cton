f00dcb4cad7e15a4b682b72ed8afde5935a4ce01
Akatsuki <1262917464@qq.com>
2021-01-25 00:42:48 +0800

    cton_llib_memcpy cton_llib_memset cton_llib_ctrncmp cton_llib_align cton_hash_remove_item cton_hash_insert_item

--- src/cton.c
+++ src/cton.c
@@ -71,7 +71,7 @@ static void * cton_float64_getptr(cton_ctx *ctx, cton_obj *obj);
  ******************************************************************************/
 
 /*
- * cton_util_memcpy()
+ * cton_llib_memcpy()
  *
  * DESCRIPTION
  *   挂名CTON的memcpy套娃，
@@ -83,29 +83,69 @@ static void * cton_float64_getptr(cton_ctx *ctx, cton_obj *obj);
  *   n: 长度（字节数）
  *
  * RETURN
- *   与memcpy定义一致.
- *
- * ERRORS
- *   与memcpy定义一致.
- */
+ *   void *dst.
+*/
 static void * cton_llib_memcpy(void *dst, const void *src, size_t n)
 {
     return memcpy(dst, src, n);
 }
 
-
+/*
+ * cton_llib_memset()
+ *
+ * DESCRIPTION
+ *   挂名CTON的memset套娃，
+ *   从b开始的len个字符替换成c.
+ *
+ * PARAMETER
+ *   b: 被粘贴的内存起始位置
+ *   c: 一个整数，会被转换成unsigned char
+ *   len: 长度（字节数）
+ *
+ * RETURN
+ *   void *b.
+ */
 static void * cton_llib_memset(void *b, int c, size_t len)
 {
     return memset(b, c, len);
 }
 
-
+/*
+ * cton_llib_strncmp()
+ *
+ * DESCRIPTION
+ *   挂名CTON的strncmp套娃，
+ *   s1和s2前n个字符进行字符串比较
+ *
+ * PARAMETER
+ *   s1: 一个字符串
+ *   s2: 另一个字符串
+ *   n: 需要比较的长度（字节数）
+ *
+ * RETURN
+ *  当s1<s2时，返回一个负数；
+ *	当s1=s2时，返回0；
+ *	当s1>s2时，返回一个正数。
+ */
 static int cton_llib_strncmp(const char *s1, const char *s2, size_t n)
 {
     return strncmp(s1, s2, n);
 }
 
-
+/*
+ * cton_llib_align()
+ *
+ * DESCRIPTION
+ *   好像是对齐用的东西，
+ *   返回需要的空间大小数值
+ *
+ * PARAMETER
+ *   size: 至少需要的空间大小
+ *   align: 空间大小为这个参数的整数倍
+ *
+ * RETURN
+ *  返回的数值不小于size，且能被align整除
+ */
 static size_t cton_llib_align(size_t size, size_t align)
 {
     size_t remain;
@@ -1178,6 +1218,17 @@ static cton_hash_item *cton_hash_search(cton_obj *h, cton_obj *k)
     return current;
 }
 
+/*
+ * cton_hash_remove_item()
+ *
+ * DESCRIPTION
+ *   CTON用hash表项目删除，
+ *   在一个哈希表里面删除一个值.
+ *
+ * PARAMETER
+ *   hash: 一个CTON哈希表
+ *   item: 一个CTON值
+ */
 static void cton_hash_remove_item(cton_ctx *ctx,
     cton_obj *hash, cton_hash_item *item)
 {
@@ -1217,6 +1268,17 @@ static void cton_hash_remove_item(cton_ctx *ctx,
     hash->payload.hash.count -= 1;
 }
 
+/*
+ * cton_hash_insert_item()
+ *
+ * DESCRIPTION
+ *   CTON用hash表项目插入，
+ *   在一个哈希表里面插入一个值.
+ *
+ * PARAMETER
+ *   hash: 一个CTON哈希表
+ *   item: 一个CTON值
+ */
 static void cton_hash_insert_item(cton_ctx *ctx,
     cton_obj *hash, cton_hash_item *item)
 {
