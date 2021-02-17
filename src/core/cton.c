5d3de5fb206122816f23d35a95b16a64803c1c02
Yeonji Lee <yeonji@short-circuits.org>
2021-02-17 12:27:46 +0900

    Sрlїт стои llїв

--- src/core/cton.c
+++ src/core/cton.c
@@ -21,6 +21,7 @@
  ******************************************************************************/
 
 #include <core/cton_core.h>
+#include <core/cton_llib.h>
 #include <limits.h>
 
 static void  cton_string_init(cton_ctx *ctx, cton_obj *str);
@@ -56,102 +57,6 @@ static void * cton_float8_getptr(cton_ctx *ctx, cton_obj *obj);
 static void * cton_float16_getptr(cton_ctx *ctx, cton_obj *obj);
 static void * cton_float32_getptr(cton_ctx *ctx, cton_obj *obj);
 static void * cton_float64_getptr(cton_ctx *ctx, cton_obj *obj);
-
-
-/*******************************************************************************
- * CTON Low level functions
- *
- *   These functions is used by cton as low level functions. some of them is
- * supposed to be offered by standard library, but for compatibily, we create
- * an abstruct of these functions.
- * 
- ******************************************************************************/
-
-/*
- * cton_llib_memcpy()
- *
- * DESCRIPTION
- *   挂名CTON的memcpy套娃，
- *   从src开始复制n个字符到dst.
- *
- * PARAMETER
- *   dst: 被粘贴的内存起始位置
- *   src: 复制源的内存起始位置
- *   n: 长度（字节数）
- *
- * RETURN
- *   void *dst.
-*/
-static void * cton_llib_memcpy(void *dst, const void *src, size_t n)
-{
-    return memcpy(dst, src, n);
-}
-
-/*
- * cton_llib_memset()
- *
- * DESCRIPTION
- *   挂名CTON的memset套娃，
- *   从b开始的len个字符替换成c.
- *
- * PARAMETER
- *   b: 被粘贴的内存起始位置
- *   c: 一个整数，会被转换成unsigned char
- *   len: 长度（字节数）
- *
- * RETURN
- *   void *b.
- */
-static void * cton_llib_memset(void *b, int c, size_t len)
-{
-    return memset(b, c, len);
-}
-
-/*
- * cton_llib_strncmp()
- *
- * DESCRIPTION
- *   挂名CTON的strncmp套娃，
- *   s1和s2前n个字符进行字符串比较
- *
- * PARAMETER
- *   s1: 一个字符串
- *   s2: 另一个字符串
- *   n: 需要比较的长度（字节数）
- *
- * RETURN
- *  当s1<s2时，返回一个负数；
- *	当s1=s2时，返回0；
- *	当s1>s2时，返回一个正数。
- */
-static int cton_llib_strncmp(const char *s1, const char *s2, size_t n)
-{
-    return strncmp(s1, s2, n);
-}
-
-/*
- * cton_llib_align()
- *
- * DESCRIPTION
- *   好像是对齐用的东西，
- *   返回需要的空间大小数值
- *
- * PARAMETER
- *   size: 至少需要的空间大小
- *   align: 空间大小为这个参数的整数倍
- *
- * RETURN
- *  返回的数值不小于size，且能被align整除
- */
-static size_t cton_llib_align(size_t size, size_t align)
-{
-    size_t remain;
-    size_t aligned;
-
-    remain = size % align;
-    aligned = size ^ remain;
-    return remain > 0 ? aligned + align : aligned ;
-}
 
 
 /*******************************************************************************
